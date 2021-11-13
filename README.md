c462770829ffd075cb24d33d5022be951cd9d81f
dhondup.lama <dhondup.lama@yipl.com.np>
2021-11-13 11:32:01 +0545

    Boilerplate setup

---
+++ README.md
@@ -0,0 +1,9 @@
+A starter kit build with Parcel to quickly start creating website UI.
+
+Includes:
+
+- HTML, CSS, JS
+- SCSS
+- PostCSS
+- Tailwind (with JIT)
+- Stylelint and Prettier to maintain code standard.
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./dist/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
